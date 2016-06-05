e3570dd5b79a19ed9e957930af6f59d930873ef4
Antiklos <chevellex@gmail.com>
2016-06-04 19:44:24 -0700

    get rid of more redundant dependencies

--- main.c
+++ main.c
@@ -1,20 +1,10 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <unistd.h>
-#include <syslog.h>
 #include <glib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/un.h>
-#include <time.h>
 
 #include "main.h"
 #include "link_test.c"
